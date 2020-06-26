5d8a018487402da2b99cde15d192d64d64a7c42c
jackyyyzzz <jacky12311@gmail.com>
2020-06-26 00:17:27 -0400

    update script to fetch in photos

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const imageList = fs.readdirSync('./src/images/display-pictures/Baskets');
 const newImgList = imageList.map((image) => {
-    image = image.slice(0, -4)
-    return image = image.split(',')
-})
+  image = image.slice(0, -4);
+  return image;
+});
 
-console.log(newImgList)
+console.log(newImgList);
